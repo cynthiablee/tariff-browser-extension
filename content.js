f31cf7f544145984817161cc2d2f30a1b9c84e8f
Cynthia <cynthia.b.lee@gmail.com>
2018-03-03 16:09:56 -0800

    updated phrases to replace for new version

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/computational thinking/gi, 'Santa Claus');
+            var replacedText = text.replace(/tariff/gi, 'indirect tax that Americans will have to pay on things they consume');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
